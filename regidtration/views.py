fc51ae8292d0a01e4ac5f483f22673b98fcf9d09
Emil <emildev09@gmail.com>
2025-01-25 13:35:11 +0100

    Fix logout and update UI

--- regidtration/views.py
+++ regidtration/views.py
@@ -1,27 +1,28 @@
-from django.shortcuts import render, redirect
-from django.http import JsonResponse, Http404, HttpResponseNotFound, HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse, HttpResponseNotFound, HttpResponseRedirect, HttpResponse
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import UserCreationForm
-import json
-from django.contrib.auth.forms import *
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import User
-from django.contrib import messages
 from django.template.loader import render_to_string
-from .forms import UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
+import json
+
+from .forms import UserUpdateForm, ProfileUpdateForm, RegistrationForm, AddFriendsForm
 from .models import Profile, Game
-from regidtration.forms import RegistrationForm, UserUpdateForm, SignUpForm, ProfileUpdateForm, AddFriendsForm
-from django.shortcuts import get_object_or_404
+
 
 def index(request):
     return render(request, "regidtration/index.html")
 
-def pvp(request, Profile):
-    return render(request, "regidtration/index.html", Profile)
+
+def pvp(request, profile):
+    return render(request, "regidtration/index.html", {'profile': profile})
+
 
 def register_done(request):
     return render(request, "regidtration/register_done.html")
+
+
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -39,6 +40,7 @@ def register(request):
         html = render_to_string('regidtration/register.html', {'form': form}, request=request)
         return JsonResponse({'success': True, 'html': html})
 
+
 def user_login(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -55,14 +57,17 @@ def user_login(request):
         html = render_to_string('regidtration/login.html', {}, request=request)
         return JsonResponse({'success': True, 'html': html})
 
-@login_required()
+
+@login_required
 def user_logout(request):
     logout(request)
     return redirect("index")
 
+
+@login_required
 def update_profile(request):
     user = request.user
-    profile = Profile.objects.get(user=request.user)
+    profile = Profile.objects.get(user=user)
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
@@ -72,8 +77,7 @@ def update_profile(request):
             messages.success(request, 'Your account has been updated!')
             return redirect('index')
         else:
-            errors = u_form.errors, p_form.errors
-            messages.error(request, 'There are errors updating your profile, please try again with another one')
+            messages.error(request, 'There are errors updating your profile, please try again.')
             return redirect('index')
     else:
         u_form = UserUpdateForm(instance=user)
@@ -86,6 +90,8 @@ def update_profile(request):
     html = render_to_string('regidtration/update_profile.html', context, request=request)
     return JsonResponse({'success': True, 'html': html})
 
+
+@login_required
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(data=request.POST, user=request.user)
@@ -95,7 +101,7 @@ def change_password(request):
             messages.success(request, 'Your password was successfully updated!')
             return redirect('index')
         else:
-            messages.error(request, 'Please correct the error in form.')
+            messages.error(request, 'Please correct the error in the form.')
             return redirect('index')
     else:
         form = PasswordChangeForm(user=request.user)
@@ -103,6 +109,7 @@ def change_password(request):
         return JsonResponse({'success': True, 'html': html})
 
 
+@login_required
 def add_friend(request):
     if request.method == 'POST':
         user_profile = Profile.objects.get(user=request.user)
@@ -112,36 +119,39 @@ def add_friend(request):
             try:
                 friend_profile = get_object_or_404(Profile, display_name=friend_name)
                 if user_profile == friend_profile:
-                    messages.error(request, 'You are cannot add yourself')
+                    messages.error(request, 'You cannot add yourself as a friend.')
                     return redirect('index')
                 elif friend_profile in user_profile.friends.all():
-                    messages.error(request, 'You are already friend')
+                    messages.error(request, 'You are already friends.')
                     return redirect('index')
                 else:
                     user_profile.friends.add(friend_profile)
                     user_profile.save()
-                    messages.success(request, 'You are now friend')
+                    messages.success(request, 'You are now friends.')
                     return redirect('index')
             except Exception as e:
-                messages.error(request, 'Something went wrong')
+                messages.error(request, 'Something went wrong while adding friend.')
                 return redirect('index')
         else:
-            messages.error(request, 'Please correct the error in form.')
+            messages.error(request, 'Please correct the error in the form.')
             return redirect('index')
     else:
         form = AddFriendsForm()
         html = render_to_string('regidtration/add_friend.html', {'form': form}, request=request)
         return JsonResponse({'success': True, 'html': html})
+
+
 @login_required
 def delete_friend(request, f_id):
     if request.method == 'POST':
         friend = get_object_or_404(Profile, id=f_id)
-        friend.delete()
-        messages.success(request, 'You are no longer friend')
+        request.user.profile.friends.remove(friend)
+        messages.success(request, 'You are no longer friends.')
         return redirect('index')
     else:
-        messages.error(request, 'Please correct the error in form.')
+        messages.error(request, 'Invalid request.')
         return redirect('index')
+
 
 @login_required
 def friend_list(request):
@@ -149,6 +159,7 @@ def friend_list(request):
     friends = user_profile.friends.all()
     statuses = {friend.display_name: friend.is_online for friend in friends}
     return JsonResponse({'statuses': statuses})
+
 
 @login_required
 def game_setup(request):
@@ -170,10 +181,11 @@ def game_setup(request):
             }
             return redirect('index')
         else:
-            messages.error(request, "Wrong credential for Player 2")
+            messages.error(request, "Wrong credentials for Player 2")
             return redirect('index')
     html = render_to_string('regidtration/game_setup.html', {}, request=request)
     return JsonResponse({'success': True, 'html': html})
+
 
 def logout_player2(request):
     if 'player2' in request.session:
@@ -183,20 +195,22 @@ def logout_player2(request):
         messages.error(request, "No Player 2 is currently logged in.")
     return redirect('index')
 
+
 def save_game_result(request):
     if request.method == 'POST':
         try:
-            print(request.body)
             data = json.loads(request.body)
             game_type = data.get('game_type')
             winner_id = data.get('winner_id')
             player2id = data.get('player2id')
-            player2 = Profile.objects.get(id = player2id)
-            player2.update_stats(won = 0)
-            winner = Profile.objects.get(id = winner_id)
-            winner.update_stats(won = 1)
-            game = Game.objects.create(game_type=game_type, winner=winner, player2=player2)
+            player2 = Profile.objects.get(id=player2id)
+            winner = Profile.objects.get(id=winner_id)
+            
+            player2.update_stats(won=0)
+            winner.update_stats(won=1)
+            
+            Game.objects.create(game_type=game_type, winner=winner, player2=player2)
             return JsonResponse({'success': True, 'message': 'Game result saved successfully!'})
         except Exception as e:
-            return JsonResponse({'success': False, 'message': "Fuck"}, status=400)
+            return JsonResponse({'success': False, 'message': "Error saving game result."}, status=400)
     return JsonResponse({'success': False, 'message': 'Invalid request'}, status=400)
